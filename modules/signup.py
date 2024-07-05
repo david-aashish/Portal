d938ab44f830e7fd75c38a9c4dd406c437b8f62f
aashish <cdavidaashish@gmail.com>
2024-07-05 14:44:51 +0530

    Initial Commit

---
+++ modules/signup.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, SubmitField, StringField
+from wtforms.validators import DataRequired, Optional, Regexp, EqualTo
+
+class Signupform(FlaskForm):
+    firstname = StringField("First Name",validators=[DataRequired()])
+    middlename = StringField("Middle Name",validators=[Optional()])
+    lastname = StringField("Last Name",validators=[DataRequired()])
+    mobilenumber = StringField("Mobile Number",validators=[DataRequired(),Regexp(regex=r'^\d+$')])
+    email = EmailField("Email" ,validators=[DataRequired()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    confirmpassword = PasswordField("Confirm Password",validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField("Sign-up")
